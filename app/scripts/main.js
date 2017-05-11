09b7df14bce24d3e3979547e4de87719516047b8
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-05-11 20:03:06 +0200

    some fixes

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,6 +47,92 @@
             fixedContentPos: false
         });
 
+        // Form
+        var welcomeForm = document.getElementById('register-form');
+        if (welcomeForm) {
+          if (welcomeForm.attachEvent) {
+              welcomeForm.attachEvent('submit', processWelcomeForm);
+          } else {
+              welcomeForm.addEventListener('submit', processWelcomeForm);
+          }
+        }
+
+        function processWelcomeForm(e) {
+          if (e.preventDefault) e.preventDefault();
+
+          try{
+            var form = document.getElementById('register-form');
+            var email = form.querySelector('#inputEmail').value;
+            var policy = form.querySelector('#policy').checked;
+
+            if(email && policy){
+              form.querySelector('#inputEmail').style.borderColor = 'inherit';
+              form.querySelector('#policy').style.borderColor = 'inherit';
+              var data = {email:email};
+              var leadFormButton = document.getElementById('lead-form-button');
+              leadFormButton.style.display = 'none';
+              var processing = document.getElementById('processing-form');
+              processing.style.display = 'block';
+              try{
+                // SendMail
+                sendEmail('c348a464-4240-4cf4-9c88-7b2c892070d7', data);
+
+                // Track Event
+                dataLayer.push({'event': 'lead_real'});
+
+                processing.style.display = 'none';
+                var success = document.getElementById('success-form');
+                success.style.display = 'block';
+              }
+              catch(e){}
+            }
+            else {
+              if (!email || email === '') {
+                form.querySelector('#inputEmail').style.borderColor = 'red';
+              }
+              if (!policy) {
+                form.querySelector('#policy').style.borderColor = 'red';
+              }
+            }
+          } catch(e){}
+          return false;
+        }
+
+       function sendEmail(id, data){
+         var url = 'https://notifications.api.willysbrewing.com/mail/send';
+         var q = new XMLHttpRequest();
+         q.open('POST', url, true);
+         q.setRequestHeader('Content-Type', 'application/json');
+         q.onreadystatechange = function(){
+           if(this.readyState === 4){
+             if(this.status.toString()[0] == '2'){
+               // ok
+             }
+             else if(this.status.toString()[0] == '4' || this.status.toString()[0] == '5'){
+               // error
+             }
+             else{
+               // foo
+             }
+           }
+         };
+         var payload = {
+           'recipient': data.email,
+           'subject': data.subject || '',
+           'content': data.content || '',
+           'template':{
+             'id': id,
+             'data':{
+               'name': data.name || 'notset',
+               'from': data.from || 'notset',
+               'content': data.content || 'notset'
+             }
+           }
+         };
+         q.send(JSON.stringify(payload));
+       }
+
+
         // Do not delay load of page with async functionality: Wait for window load
         window.addEventListener('load',function(){
 
