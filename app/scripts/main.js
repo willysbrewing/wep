a2f998215ba1549e924b29735c07a343d9a0e6ef
Sergio Gordillo <sgordillogallardo@gmail.com>
2017-05-12 17:55:22 +0200

    ready? yep!

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -47,6 +47,17 @@
             fixedContentPos: false
         });
 
+        var colors = ['#FFFF00','#FFFF33','#F2EA02','#E6FB04',
+        '#FF0000','#FD1C03','#FF3300','#FF6600',
+        '#00FF00','#00FF33','#00FF66','#33FF00',
+        '#00FFFF','#099FFF','#0062FF','#0033FF',
+        '#FF00FF','#FF00CC','#FF0099','#CC00FF',
+        '#9D00FF','#CC00FF','#6E0DD0','#9900FF'];
+
+        setInterval(function(){
+          $('.everlasting').css('color', colors[(Math.trunc(Math.random()*colors.length))]);
+        }, 500);
+
         // Form
         var welcomeForm = document.getElementById('register-form');
         if (welcomeForm) {
